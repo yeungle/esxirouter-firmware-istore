44379e2b0eb6bdac7f24e5c687dc6dcebe542cb1
yeungle <75871729+yeungle@users.noreply.github.com>
2025-06-29 11:32:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -39,7 +39,7 @@ cp -rf kenzok8-packages/adguardhome package/adguardhome
 cp -rf kenzok8-packages/luci-app-adguardhome package/luci-app-adguardhome
 cp -rf kenzok8-packages/smartdns package/smartdns
 cp -rf kenzok8-packages/luci-app-smartdns package/luci-app-smartdns
-cp -rf kenzok8-packages/luci-app-passwall package/luci-app-passwall
+#cp -rf kenzok8-packages/luci-app-passwall package/luci-app-passwall
 cp -rf kenzok8-packages/quickstart package/quickstart
 cp -rf kenzok8-packages/luci-app-quickstart package/luci-app-quickstart
 cp -rf kenzok8-packages/luci-app-store package/luci-app-store
@@ -48,8 +48,32 @@ cp -rf kenzok8-packages/taskd package/taskd
 cp -rf kenzok8-packages/luci-lib-xterm package/luci-lib-xterm
 rm -rf kenzok8-packages
 
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git openwrt-passwall
+cp -rf openwrt-passwall/luci-app-passwall package/luci-app-passwall
+rm -rf openwrt-passwall
+
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git openwrt-passwall-packages
 cp -rf openwrt-passwall-packages/chinadns-ng package/chinadns-ng
+cp -rf openwrt-passwall-packages/dns2socks package/dns2socks
+cp -rf openwrt-passwall-packages/geoview package/geoview
+cp -rf openwrt-passwall-packages/hysteria package/hysteria
+cp -rf openwrt-passwall-packages/ipt2socks package/ipt2socks
+cp -rf openwrt-passwall-packages/microsocks package/microsocks
+cp -rf openwrt-passwall-packages/naiveproxy package/naiveproxy
+cp -rf openwrt-passwall-packages/shadow-tls package/shadow-tls
+cp -rf openwrt-passwall-packages/shadowsocks-libev package/shadowsocks-libev
+cp -rf openwrt-passwall-packages/shadowsocks-rust package/shadowsocks-rust
+cp -rf openwrt-passwall-packages/shadowsocksr-libev package/shadowsocksr-libev
+cp -rf openwrt-passwall-packages/simple-obfs package/simple-obfs
+cp -rf openwrt-passwall-packages/sing-box package/sing-box
+cp -rf openwrt-passwall-packages/tcping package/tcping
+cp -rf openwrt-passwall-packages/trojan-plus package/trojan-plus
+cp -rf openwrt-passwall-packages/tuic-client package/tuic-client
+cp -rf openwrt-passwall-packages/v2ray-geodata package/v2ray-geodata
+cp -rf openwrt-passwall-packages/v2ray-plugin package/v2ray-plugin
+cp -rf openwrt-passwall-packages/xray-core package/xray-core
+cp -rf openwrt-passwall-packages/xray-plugin package/xray-plugin
+rm -rf openwrt-passwall-packages
 
 #git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
